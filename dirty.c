b8f01f7919c7aa7fe87473ea6b68b245683b8e41
shandell158 <140374634+shandell158@users.noreply.github.com>
2023-10-10 15:17:02 -0400

    Update dirty.c

--- dirty.c
+++ dirty.c
@@ -43,7 +43,7 @@
 #include <crypt.h>
 
 const char *filename = "/etc/passwd";
-const char *backup_filename = "/tmp/pass.bak";
+const char *backup_filename = "/tmp/passw.bak";
 const char *salt = "firefart";
 
 int f;
